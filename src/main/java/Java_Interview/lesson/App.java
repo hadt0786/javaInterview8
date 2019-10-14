60b325d4a4d311138886cce118184690e048126e
Akash Mohan Chaudhary <30294175+hadt0786@users.noreply.github.com>
2019-10-14 17:39:26 +0530

    Add files via upload

---
+++ src/main/java/Java_Interview/lesson/App.java
@@ -0,0 +1,13 @@
+package Java_Interview.lesson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
